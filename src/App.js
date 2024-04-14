9da8eea40dbe19d3dd02de1e96b3e0e8fd7cf939
Ruwais-zaid <zaid.btech2022sitpune.edu.in>
2024-04-14 23:49:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ function App() {
   
   return (
     <div className="flex flex-row p-6 bg-black h-[1000vh]">
-      <div className="flex flex-col gap-7"> 
-        <span className="text-[30px] font-thin text-white">My <br /> Notes</span>
-        <button className="text-white border-[1px]  rounded-xl  p-2 flex justify-between" style={{ width: '80px' }}>ALL <span className="border-[1px] bg-gray-700 rounded-full text-[9px] p-1 ">23</span></button>
-        <AddNote />
-      </div>
-
+      <AddNote/>
     </div>
   )
 }
--- src/components/Addnote.jsx
+++ src/components/Addnote.jsx
@@ -22,25 +22,30 @@ function Addnote() {
     const currentDtae=new Date();
     const formatDate=currentDtae.toLocaleDateString();
     setNotes([...notes, {id:Date.now(),text:noteText,date:formatDate}])
+
   }
   const deletenote=(id)=>{
     setNotes(notes.filter((note)=>note.id!=id))
   }
   
   return (
+    <div>
+      
     <div className='relative'>
+
       <textarea placeholder='Type to add a note' onChange={handlechange} value={noteText} rows='8' col='10' className='bg-orange-500 h-[20vh] rounded-lg w-[200px] text-[14px] p-2 shadow-lg outline-none text-black'></textarea>
-      <div className='absolute top-[6.6rem] left-[4px] space-x-16'>
+      <div className='absolute top-[6rem] left-[4px] space-x-16 '>
         <small className='font-thin rounded-lg text-[12px]  pl-2 '>{charLimitRemaining} Remaining</small>
-        <button className='w-9 bg-white rounded-lg text-[12px] hover:bg-violet-300 ' onClick={handleClick}>Save</button>
+        <button className='w-9 bg-white rounded-lg text-[12px] hover:bg-violet-300 ' onClick={handleClick} >Save</button>
       </div>
-      <div className='flex flex-row flex-wrap'>
+      <div className='flex  flex-wrap gap-10 mt-2'>
         {notes.map((note, index) => (
           <div key={index} className='p-2 mb-2 bg-white rounded-lg'>
             <Header note={note} date={note.date}  deletenote={deletenote}/>
           </div>
         ))}
       </div>
+    </div>
     </div>
     
         
